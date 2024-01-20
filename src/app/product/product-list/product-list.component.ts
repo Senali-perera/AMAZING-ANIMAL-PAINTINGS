9e2ca5131df42d9bbf6ba6cef1a44c4462b6a090
Senali-perera <senaliperera1996@gmail.com>
2024-01-20 21:56:05 +0200

    View the product list

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {ProductService} from "../product.service";
+import {Product} from "../../models/product";
 
 @Component({
   selector: 'app-product-list',
@@ -7,6 +9,16 @@ import { Component } from '@angular/core';
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit{
 
+  products: Product[] = []
+
+  constructor(private productService:ProductService){
+
+  }
+  ngOnInit() {
+    this.productService.getProducts().subscribe(data =>{
+      this.products = data;
+    });
+  }
 }
