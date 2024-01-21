f882dded80ccb15649e8b52d573fd15300b01fe6
Senali-perera <senaliperera1996@gmail.com>
2024-01-21 21:44:39 +0200

    Get all the cart items

--- src/app/cart/cart-view/cart-view.component.ts
+++ src/app/cart/cart-view/cart-view.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {CartService} from "../cart.service";
+import {Product} from "../../models/product";
+import {MatCardModule} from "@angular/material/card";
+import {MatListModule} from "@angular/material/list";
+import {MatButtonModule} from "@angular/material/button";
 
 @Component({
   selector: 'app-cart-view',
@@ -7,6 +12,16 @@ import { Component } from '@angular/core';
   templateUrl: './cart-view.component.html',
   styleUrl: './cart-view.component.css'
 })
-export class CartViewComponent {
+export class CartViewComponent implements OnInit {
 
+  cartItems: Product[] = [];
+
+  constructor(private cartService: CartService) {
+  }
+
+  ngOnInit() {
+    this.cartService.getCartItems().subscribe(data=>{
+      this.cartItems = data
+    })
+  }
 }
