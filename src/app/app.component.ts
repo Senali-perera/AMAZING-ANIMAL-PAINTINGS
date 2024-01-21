34977e99af0ebe5e1ca9120ed8608a51363f9c14
Senali-perera <senaliperera1996@gmail.com>
2024-01-21 17:02:05 +0200

    Adding a navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {MatToolbar, MatToolbarRow} from '@angular/material/toolbar';
+import {MatAnchor} from "@angular/material/button";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MatToolbarRow, MatToolbar, MatAnchor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
